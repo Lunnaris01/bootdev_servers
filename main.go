966e28a76643c289345eb66e811f2ddfb344f1ef
Lunnaris01 <Julian_Ullrich@live.de>
2025-02-11 15:43:58 +0100

    added namespace

--- main.go
+++ main.go
@@ -2,7 +2,45 @@ package main
 
 import (
 	"net/http"
+	"sync/atomic"
+	"fmt"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w,r)
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, req *http.Request){
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(200)
+	content_html := 
+	`<html>
+		<body>
+			<h1>Welcome, Chirpy Admin</h1>
+			<p>Chirpy has been visited %d times!</p>
+		</body>
+	</html>`
+	content_to_serve := fmt.Sprintf(content_html,cfg.fileserverHits.Load())
+	w.Write([]byte(content_to_serve))
+
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, req *http.Request){
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("Reset successfull"))
+
+}
+
 
 func main(){
 	serveMux := http.NewServeMux()
@@ -10,9 +48,12 @@ func main(){
 		Handler: serveMux,
 		Addr: "localhost:8080",
 	}
+	apiCfg := apiConfig{}
 
-	serveMux.Handle("/app/",http.StripPrefix("/app/",http.FileServer(http.Dir("."))))
-	serveMux.HandleFunc("/healthz",healthHandler)
+	serveMux.Handle("/app/",http.StripPrefix("/app/",apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
+	serveMux.HandleFunc("GET /api/healthz",healthHandler)
+	serveMux.HandleFunc("GET /admin/metrics",apiCfg.metricsHandler)
+	serveMux.HandleFunc("POST /admin/reset",apiCfg.resetHandler)
 	server.ListenAndServe()
 
 
@@ -24,3 +65,4 @@ func healthHandler(w http.ResponseWriter, req *http.Request){
 	w.WriteHeader(200)
 	w.Write([]byte("OK"))
 }
+
