7b4d94058f9902e6126d4c0504531d2d938b2825
Lunnaris01 <Julian_Ullrich@live.de>
2025-02-12 02:58:42 +0100

    postgres/psql/goose setup

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/Lunnaris01/bootdev_servers
 
 go 1.23.5
+
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
--- main.go
+++ main.go
@@ -4,6 +4,10 @@ import (
 	"net/http"
 	"sync/atomic"
 	"fmt"
+	"encoding/json"
+	"io"
+	"strings"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
@@ -41,6 +45,64 @@ func (cfg *apiConfig) resetHandler(w http.ResponseWriter, req *http.Request){
 
 }
 
+func (cfg *apiConfig) validateChirpHandler (w http.ResponseWriter, req *http.Request){
+	type http_body struct{
+		Body string `json:"body"`
+		Error string `json:"error"`
+		Valid bool `json:"valid"`
+		CleanedBody string `json:"cleaned_body"`
+	}
+
+	r_body := http_body{}
+	r_data, err := io.ReadAll(req.Body)
+	if err != nil {
+		response_body := http_body{
+			Error: "Something went wrong",
+		}	
+		response_json, _ := json.Marshal(response_body)
+		w.WriteHeader(400)
+		w.Write([]byte(response_json))
+		return
+	}
+	defer req.Body.Close()
+	err = json.Unmarshal(r_data,&r_body)
+	if err != nil {
+		response_body := http_body{
+			Error: "Something went wrong",
+		}	
+		response_json, _ := json.Marshal(response_body)
+		w.WriteHeader(400)
+		w.Write(response_json)
+		return
+	}
+	r_body.CleanedBody = clean_chirp(r_body.Body)
+	if len(r_body.CleanedBody)>140{
+		response_body := http_body{
+			Error: "Chirp is too long",
+		}	
+		response_json, _ := json.Marshal(response_body)
+		w.WriteHeader(400)
+		w.Write(response_json)
+		return
+	}
+	response_body := http_body{
+		CleanedBody: r_body.CleanedBody,
+		Valid: true,
+	}	
+	response_json, _ := json.Marshal(response_body)
+	w.WriteHeader(200)
+	w.Write(response_json)
+}
+
+func clean_chirp(input_message string) string {
+	badwords := []string{"kerfuffle","sharbert","fornax"}
+	for _,word := range badwords{
+		input_message = strings.ReplaceAll(input_message,word,"****")
+		input_message = strings.ReplaceAll(input_message,strings.Title(word),"****")
+	}
+	return input_message
+}
+
 
 func main(){
 	serveMux := http.NewServeMux()
@@ -54,6 +116,7 @@ func main(){
 	serveMux.HandleFunc("GET /api/healthz",healthHandler)
 	serveMux.HandleFunc("GET /admin/metrics",apiCfg.metricsHandler)
 	serveMux.HandleFunc("POST /admin/reset",apiCfg.resetHandler)
+	serveMux.HandleFunc("POST /api/validate_chirp",apiCfg.validateChirpHandler)
 	server.ListenAndServe()
 
 
